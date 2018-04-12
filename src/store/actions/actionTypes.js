b8a2485a9d722ef6c0a8ebef9a92ffac034ceb87
Egor Piskunou2 <egor.piskunou@gmail.com>
2018-04-12 16:02:00 +0300

    add action types

---
+++ src/store/actions/actionTypes.js
@@ -0,0 +1,15 @@
+export const TOGGLE_DHCP = 'TOGGLE_DHCP';
+export const CHANGE_IP = 'CHANGE_IP';
+export const CHANGE_MASK = 'CHANGE_MASK';
+export const CHANGE_GATEWAY = 'CHANGE_GATEWAY';
+export const TOGGLE_DNS = 'TOGGLE_DNS';
+export const CHANGE_DNS_IP = 'CHANGE_DNS_IP';
+export const CHANGE_ALT_DNS_IP = 'CHANGE_ALT_DNS_IP';
+export const TOGGLE_WIFI = 'TOGGLE_WIFI';
+export const TOGGLE_WIFI_SECURITY = 'TOGGLE_WIFI_SECURITY';
+export const CHANGE_SEC_KEY = 'CHANGE_SEC_KEY';
+export const SELECT_NETWORK = 'SELECT_NETWORK';
+
+export const GET_FORM_STATE = 'GET_FORM_STATE';
+export const GET_AVAILABLE_NETWORKS = 'GET_AVAILABLE_NETWORKS';
+export const SUBMIT_FORM = 'SUBMIT_FORM';
